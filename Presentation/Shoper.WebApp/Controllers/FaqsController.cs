84a04458a5fdea6f3cc9fd1ba4d8e0d49959da7c
Muhammet Seyda Armağan <muhammedseyda@hotmail.com>
2024-11-01 22:17:06 +0300

    sss contact hhelp sayfalari

---
+++ Presentation/Shoper.WebApp/Controllers/FaqsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shoper.WebApp.Controllers
+{
+    public class FaqsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
